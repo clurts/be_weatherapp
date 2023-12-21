c5ccaaebaa7077e19a6832c74b3369852716e2d4
Carsten Lund <clu@rts.dk>
2023-12-21 08:26:22 +0100

    playin around

--- src/components/weather-front.js
+++ src/components/weather-front.js
@@ -13,7 +13,7 @@ export default function WeatherFront() {
     <article className="flex flex-col items-center">
       {loading ? <p>loading</p> : 
       <>
-        <p className="text-3xl text-darkCompBg">{data?.name}</p>
+        <p className="text-3xl text-white">{data?.name}</p>
         <p className="text-[5em] text-white">{Math.round(data?.main.temp - 272.15)}&deg;</p>
         {/* <Image src={`/icons/${data?.weather[0].icon}.svg`} width={100} height={100} alt="" /> */}
         <p className="text-xl text-gray-300 capitalize">{data?.weather[0].description}</p>
