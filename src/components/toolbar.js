fadb69e17dc79a03a6faceef7b4376d0033fa6e6
Brian Emilius <be@rts.dk>
2024-01-02 09:33:44 +0100

    fix spelling error

--- src/components/toolbar.js
+++ src/components/toolbar.js
@@ -4,7 +4,7 @@ export default function Toolbar() {
     return (
         <div className="relative">
             <section className="bg-purple-500 absolute w-full h-[250px] z-10 bottom-0 rounded-t-2xl">
-                <p>Fnyf</p>
+                <p>Tingeling</p>
             </section>
             <nav style={{backgroundImage: "url(/toolbar.svg)"}} className="h-[100px] bg-no-repeat bg-center relative z-50">
 
